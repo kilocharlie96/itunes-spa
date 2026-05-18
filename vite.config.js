0d0b27573792100cfb3c124bf78872d477a93560
Kristian Cernak <cernak.kiko@gmail.com>
2026-05-18 11:40:53 +0200

    fix: set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/itunes-spa/',
   plugins: [
     vue(),
     vueDevTools(),
