e7b8b718e85f99c3ea3480380d1424382ca6fbec
Kristian Cernak <cernak.kiko@gmail.com>
2026-05-20 15:08:47 +0200

    style: adjust IconStar dimensions for improved visual consistency

--- src/components/icons/IconStar.vue
+++ src/components/icons/IconStar.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg viewBox="0 0 24 24" width="50" height="50" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24" width="45" height="45" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
