169bf5fdaa7c5c0fb56e35d2b4fed2c78d1aaf05
Kristian Cernak <cernak.kiko@gmail.com>
2026-05-20 19:35:55 +0200

    style: improve FoodView layout and functionality with enhanced rating system, updated button interactions, and refined input field styles

--- src/views/FoodView.vue
+++ src/views/FoodView.vue
@@ -2,8 +2,8 @@
   <div class="food">
     <form class="ean" @submit.prevent="getFood()" action="#">
       <label class="title" for="ean">iFoods</label>
-      <input class="input has-text-centered" v-model="query" type="text" inputmode="numeric" pattern="[0-9]*" autofocus name="ean"
-        placeholder="Zadaj čiarový (EAN) kód">
+      <input class="input has-text-centered" v-model="query" type="text" inputmode="numeric" pattern="[0-9]*" autofocus
+        name="ean" placeholder="Zadaj čiarový (EAN) kód">
     </form>
 
     <div>
@@ -13,12 +13,12 @@
         <li>5449000000439</li>
         <li>4056489158448</li>
       </ul>
-</div>
+    </div>
 
-    <div class="card" v-if="food.brands">
-      <div class="card-image">
+    <div class="card mb-6" v-if="food.brands">
+      <div class="card-image has-background-white is-clipped">
         <figure class="image">
-          <img :src="food.image_url" alt="food image" />
+          <img :src="food.image_url" class="is-radiusless" alt="food image" />
         </figure>
       </div>
       <div class="card-content">
@@ -36,11 +36,11 @@
           </p>
 
           <div>
-            <StarIcon :class="{ filled: food.avgRating >= 1 }"/>
-            <StarIcon :class="{ filled: food.avgRating >= 2 }"/>
-            <StarIcon :class="{ filled: food.avgRating >= 3 }"/>
-            <StarIcon :class="{ filled: food.avgRating >= 4 }"/>
-            <StarIcon :class="{ filled: food.avgRating >= 5 }"/>
+            <StarIcon :class="{ filled: food.avgRating >= 1 }" />
+            <StarIcon :class="{ filled: food.avgRating >= 2 }" />
+            <StarIcon :class="{ filled: food.avgRating >= 3 }" />
+            <StarIcon :class="{ filled: food.avgRating >= 4 }" />
+            <StarIcon :class="{ filled: food.avgRating >= 5 }" />
           </div>
 
           <p>
@@ -48,13 +48,29 @@
           </p>
 
           <form class="rating" v-if="food.brands" @submit.prevent="rate()" action="#">
-            <label for="rating">Vaše hodnotenie:</label>
+            <label for="rating">Ohodnotiť:</label>
 
-            <button class="button is-danger p-0 rating-button" :disabled="rating === 0" @click.prevent="rating--">-</button>
-            <input class="input has-text-centered" v-model="rating" type="number" min="0" max="5" name="rating">
-            <button class="button is-success p-0 rating-button" :disabled="rating === 5" @click.prevent="rating++">+</button>
+            <div class="rating-container">
+              <button class="button is-danger p-0 rating-button" @click.prevent="rating--"
+                :disabled="rating === 0 || isRated">-</button>
+              <input class="input has-text-centered" v-model="rating" type="number" min="0" max="5" name="rating"
+                :disabled="isRated">
+              <button class="button is-success p-0 rating-button" @click.prevent="rating++"
+                :disabled="rating === 5 || isRated">+</button>
+            </div>
 
-            <button class="button is-success" @submit.prevent="rate()">Odoslať</button>
+
+
+            <button class="button is-success" @submit.prevent="rate()" :disabled="isRated">
+              <svg viewBox="0 0 16 16" height="25" width="25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path d="M8 6L8 2L10 2L16 8L10 14L8 14L8 10L-1.74845e-07 10L-3.01991e-07 6L8 6Z" fill="#fff"></path>
+                </g>
+              </svg>
+            </button>
+
           </form>
         </div>
       </div>
@@ -75,7 +91,8 @@ export default {
       query: '',
       rating: 0,
       food: {},
-      quantity: ''
+      quantity: '',
+      isRated: false
     }
   },
   methods: {
@@ -97,68 +114,74 @@ export default {
         });
     },
     rate() {
+      if (this.isRated) return;
+
       this.food.totalRating += this.rating;
       this.food.numOfRatings++;
       this.food.avgRating = (this.food.totalRating / this.food.numOfRatings).toFixed(0);
 
+      this.isRated = true;
     },
   },
 }
 </script>
 
 <style scoped>
-  input[type=number] {
-    appearance: textfield;
-  }
+input[type=number] {
+  appearance: textfield;
+}
 
-  input[type=number]::-webkit-outer-spin-button,
-  input[type=number]::-webkit-inner-spin-button {
-    appearance: none;
-    margin: 0;
-  }
+input[type=number]::-webkit-outer-spin-button,
+input[type=number]::-webkit-inner-spin-button {
+  appearance: none;
+  margin: 0;
+}
 
-  .food {
-    text-align: center;
-    padding-top: 3em;
-  }
+.food {
+  text-align: center;
+  padding-top: 3em;
+}
 
-  .ean label.title {
-    display: block;
-  }
+.ean label.title {
+  display: block;
+}
 
-  .card {
-    margin: 3em auto 0 auto;
-    max-width: 25rem;
-  }
+.card {
+  margin: 3em auto 0 auto;
+  max-width: 25rem;
+}
 
-  .image {
-    margin: 0 auto;
-    width: 65%;
-  }
+img {
+  width: auto;
+  max-height: 400px;
+  margin: 0 auto;
+}
 
-  .filled {
-    fill: #f5c542;
-  }
+.filled {
+  fill: #f5c542;
+}
 
-  button.rating-button {
-    height: 1.5em;
-    width: 1.5em;
-  }
+button.rating-button {
+  height: 1.5em;
+  width: 1.5em;
+}
 
-  .rating {
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-  }
+.rating-container, .rating {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 2%;
+}
 
-  .rating label {
-    margin: auto 0;
-  }
+.rating label {
+  margin: auto 0;
+}
 
-  .ean input {
-    width: 12em;
-  }
-  .rating input {
-    width: 3em;
-  }
+.ean input {
+  width: 12em;
+}
+
+.rating input {
+  width: 3em;
+}
 </style>
